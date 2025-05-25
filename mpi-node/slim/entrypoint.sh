a1958b61a8fd2abba387e5a705468d712e5b8c19
MartinFarres <martinfarres@gmail.com>
2025-05-25 19:28:31 -0300

    Add python socket_handler to mpi-node

--- mpi-node/slim/entrypoint.sh
+++ mpi-node/slim/entrypoint.sh
@@ -8,5 +8,8 @@ if [ ! -f "/home/mpi-user/.ssh/id_rsa" ]; then
   ssh-keygen -t rsa -b 4096 -f /home/mpi-user/.ssh/id_rsa -N ''
 fi
 
+# Start the Python socket server in the background
+python3 /home/mpi-user/socker_handler.py &
+
 # Keep container running
 exec tail -f /dev/null
