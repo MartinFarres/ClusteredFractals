02f802fc7fe4346c9735bd0b085532d1e9f84d18
MartinFarres <martinfarres@gmail.com>
2025-06-05 20:09:47 -0300

    Server Service Fix - Use of ClusterIp no LoadBalancer

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = `http://10.66.100.201/api`;
+const API_URL = 'http://server/api';
 
 export const submitJob = async (params) => {
   const response = await axios.put(`${API_URL}/submit-job`, params);
